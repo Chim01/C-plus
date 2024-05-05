a344fa5cc0f45cd2840589ac8fef5d6af9848dc1
Chim01 <chimdaveo@gmail.com>
2024-05-05 14:26:54 +0100

    revert changes

--- Math/simple_intrest.cpp
+++ Math/simple_intrest.cpp
@@ -11,4 +11,10 @@ int main(){
     float time= 24;
     float rate= 0.03;
     float simple_intrest;
+
+    //calculations
+    simple_intrest= principal* time* rate;
+
+    //Print result
+    cout<<"Simple intrest= "<< (simple_intrest);
 }
