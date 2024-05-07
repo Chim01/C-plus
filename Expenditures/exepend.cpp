9ba919d16842e4972be4476f5920f409493a206b
Chim01 <chimdaveo@gmail.com>
2024-05-07 20:17:07 +0100

    modifications

--- Expenditures/exepend.cpp
+++ Expenditures/exepend.cpp
@@ -5,8 +5,6 @@ int main(){
     int income= 1000000;
     int expenditure= 0.02*income;
     int total_profit= income-expenditure;
-    cout<< "Income= ₦ "<< income<<"\n";
     cout<< "expenditure= ₦ "<< expenditure<< "\n";
-    cout<< "Profit= ₦ "<< total_profit;
     return 0;
 }
